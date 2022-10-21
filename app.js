820b8d032b85b90d388944298492509a86addbf6
Bine <hubillahubillahubilla@gmail.com>
2022-10-21 14:30:04 +0800

    init

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
+const add = require('./add')
 const write = require('./write')
 const read = require('./read')
+const present = require('./present')
+const cmd = process.argv
 
-const cmd = process.argv[2]
+var note = {}
 
-write(cmd)
+if(cmd[2] == 'add') {
+
+  note = {
+    id: cmd[3],
+    title: cmd[4],
+    body: cmd[5]
+  }
+
+  var oldNote = read()
+
+  add(note, oldNote)
+}
+if(cmd[2] == 'read') {
+    present(read())
+  }
+  if(cmd[2] == 'delete') {
+    const id = cmd[3]
+    const oldNote = read()
+    const del = require('./del')
+    del(id, oldNote)
+    
+    console.log(del)
+  }
+  
